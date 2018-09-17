1eb086d6b99269e5ddb8d9525eb1b0de22a7ef45
Praveen Puvvula <praveenpuvvula43@gmail.com>
2018-09-16 23:26:14 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var fahrenheit, celsius;
   var weatherApiUrl="https://api.openweathermap.org/data/2.5/weather";
-  var apiKey = "f524f7953da25ab633d77ae327602650";
+  var apiKey = "28d0c4f7315bc20e616616a8b3eb6d0a";
   getLatLong();
 
 function getLatLong(){
@@ -35,9 +35,9 @@ function getWeatherData(){
       fahrenheit=celsius*1.8+32;
       var icon=data.weather[0].icon;
       var weatherDetail=data.weather[0].main+", "+data.weather[0].description;
-      $('.weatherDetail').html(weatherDetail);
-      $('.iconpic>img').attr('src','http://openweathermap.org/img/w/'+icon+'.png');
-      $('.temp').html(temprature+"&#8451;");
+      $('.weatherDetail').html(weatherDetail); //update weather description in html
+      $('.iconpic>img').attr('src','http://openweathermap.org/img/w/'+icon+'.png'); //update the icon based on weather
+      $('.temp').html(temprature+"&#8451;"); //update the temprature
     },
     error: function(err) {
       alert('Oops something went wrong, Please try again.');
@@ -46,11 +46,13 @@ function getWeatherData(){
   });
 }
 $('.toggle .btn').click(function(){
+  // if the div has attribute id as c then convert temperature to fahrenheit
   if($('.toggle').attr('id')=='c'){
     $('.temp').html(fahrenheit+"&#8457;");
     $('.toggle').attr('id','f');
   }
   else if($('.toggle').attr('id')=='f'){
+    //else if div has attribute id as f than convert temperature to celsius
     $('.temp').html(celsius+"&#8451;");
     $('.toggle').attr('id','c');
   }
